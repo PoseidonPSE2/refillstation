c83259c369447ecc05d52a3700e3be5c621e0ef5
MrRestre <alexresklin@gmail.com>
2024-04-26 14:06:38 +0100

    added button functionalites and configured local backend

--- 03_main.py
+++ 03_main.py
@@ -2,63 +2,105 @@ import RPi.GPIO as GPIO
 import json
 import requests
 import time
+import logging
 
 from pn532 import *
 
-led = 18
-button = 17
+#-------------------- Hardware Configs --------------------
+# Pin numbers in Pi
+led_pin = 18
+button_pin = 17
 
-start_time = None
-button_pressed = False
+# Help variables to track button pressed time
+button_start = time.time()
+button_end = time.time()
 
+# GPIO configurations
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
-GPIO.setup(led, GPIO.OUT)
-GPIO.setup(button, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-
-def turn_on_led_for(seconds):
-    GPIO.output(led,GPIO.HIGH)
-    time.sleep(seconds)
-    GPIO.output(led,GPIO.LOW)
-    return True
-
-def button_callback(channel):
-    global start_time, button_pressed
-    if GPIO.input(button) == GPIO.LOW:
-        start_time = time.time()
-        button_pressed = True
-    else:
-        if start_time is not None:
-            duration = time.time() - start_time
-            print("Button pressed for {:.2f} seconds".format(duration))
-            start_time = None
-            button_pressed = False
-
-# Define the headers and the endpoint (specifying that the content type is JSON)
-tag_id_endpoint = "https://webhook.site/fdb9b6e1-fc04-4272-89b7-c10fbe2015fb"
-headers = {'Content-Type': 'application/json'}
+# Pin definition
+GPIO.setup(led_pin, GPIO.OUT)
+GPIO.setup(button_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 # Variable to block reading while tag read being proccessed
 card_read = False
 
-GPIO.add_event_detect(button, GPIO.BOTH, callback=button_callback, bouncetime=200)
+#-------------------- Backend Configs --------------------
 
+# Define the headers and the endpoint (specifying that the content type is JSON)
+local_endpoint = "http://localhost:8080/"
+headers = {"Content-Type": "application/json"}
 
+#-------------------- Helper functions --------------------
+
+def turn_on_led_for(seconds):
+    GPIO.output(led_pin,GPIO.HIGH)
+    time.sleep(seconds)
+    GPIO.output(led_pin,GPIO.LOW)
+    return True
+
+def button_callback(channel):
+    global button_start, button_end
+    if GPIO.input(button_pin) == 1:
+        # Start timer
+        button_start = time.time()
+        # Turn on LED
+        GPIO.output(led_pin,GPIO.HIGH)
+
+    if GPIO.input(button_pin) == 0:
+        # End timer and calculate elapsed
+        button_end = time.time()
+        elapsed = button_end - button_start
+        # Turn off LED
+        GPIO.output(led_pin,GPIO.LOW)
+        logger.debug("Button pressed for %i" % elapsed)
+        #Inform Backend
+        post_water_given("", elapsed)
+
+# To-Do add station and user id
+def post_water_given(id, seconds):
+    ml = str(seconds * 100)
+    post_endpoint = local_endpoint + "add"
+    body_object = {
+        "id": id,
+        "ml": ml,
+        "waterType": "still"
+    }
+    body_json = json.dumps(body_object)
+
+    response = requests.post(url=post_endpoint, data=body_json, headers=headers)
+    logger.debug(response)
+
+#-------------------- Init --------------------
+
+# Configure logging
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
+
+# Create a logger"
+logger = logging.getLogger(__name__)
+
+# Set the callback for button pressed
+GPIO.add_event_detect(button_pin, GPIO.BOTH, callback=button_callback, bouncetime=200)
+
+# PN532 Config
 pn532 = PN532_I2C(debug=False, reset=20, req=16)
-
 ic, ver, rev, support = pn532.get_firmware_version()
-print('Found PN532 with firmware version: {0}.{1}'.format(ver, rev))
+message = 'Found PN532 with firmware version: {0}.{1}'.format(ver, rev)
+logger.info(message)
+
 
 # Configure PN532 to communicate with MiFare cards
 pn532.SAM_configuration()
 
-print('Waiting for RFID/NFC card to read from!')
+#-------------------- Main Loop --------------------
+
+logger.info('Waiting for RFID/NFC card to read from!')
 try:
     while True:
-        if GPIO.input(button) == GPIO.HIGH:
-            print("Button was pushed!")
-
         # Check if a card is available to read
         uid = pn532.read_passive_target(timeout=0.5)
                 
@@ -68,33 +110,44 @@ try:
 
         if card_read == True:
             tag_id = ':'.join(['{:02X}'.format(byte) for byte in uid])
-            print('Found card with UID:', tag_id)
-            print('Fetching data for given id.')
+            logger.info('Found RFID-Tag with UID: %s' % tag_id)
             
-            json_request = {
-                "tag_id": tag_id
+            body_object = {
+                "ID": tag_id
             }
+            body_json = json.dumps(body_object)
 
             # Send the get request with the JSON data
-            response = requests.get(tag_id_endpoint, data=json_request, headers=headers)
-            json_response = json.loads(response.text)
+            response = requests.post(url=local_endpoint, data=body_json, headers=headers)
+            
+            try:
+                json_response = json.loads(response.text)
+                # Access the properties
+                # user_id = json_response['User_ID'] -> Not yet in backend
+                milliliter = int(json_response['ml'])
+                waterType = json_response['waterType']
 
-            # Access the properties
-            user_id = json_response['User_ID']
-            milliliter = json_response['Milliliter']
+                # Print the response
+                logger.info("Data found for id %s %s" % (tag_id, json_response))
 
-            # Print the response
-            print(json_response)
+                logger.info('Water delivery started')
 
-            # More or less one second each 100 ml
-            turn_on_led_for(milliliter / 100)
+                # More or less one second each 100 ml
+                time_on = milliliter / 100
+                turn_on_led_for(time_on)
 
-            print('Water succesfully delivered.')
+                logger.info('Water delivery finished')
+
+                #Inform Backend
+                post_water_given(tag_id, time_on)
+
+            except:
+                logger.error("server response: %s" % response)
 
             card_read = False
 
 except KeyboardInterrupt:
-    print("Program terminated by user.")
+    logger.warning("....Program terminated by user...")
     GPIO.cleanup()
 
 
